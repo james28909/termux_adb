835c1b585b5503689be9c1c49c9039604d4eedf4
james28909 <james28909@gmail.com>
2024-06-26 22:46:47 -0400

    Update termux_adb.sh

--- termux_adb.sh
+++ termux_adb.sh
@@ -9,9 +9,9 @@ pkg i iproute2 android-tools -y
 ip=$(ip addr show wlan0 | grep "inet " | awk '{print $2}' | cut -d/ -f1)
 
 #connect to and use adb shell
-adb connect $ip:5555
+#adb connect $ip:5555
 #adb -s $ip:5555 shell
-adb -s shell $ip:5555 wm user-rotation lock 1
+adb -s $ip:5555 shell wm user-rotation lock 1
 
 # you should be in an adb shell env and the shell prompt should have changed to your device's codename
 # you can execute the commands here as 'wm user-rotation lock|free 0|1' 
